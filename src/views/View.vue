a8bd612591925c8a8f998e3c1987533412603db1
cain <cainmaila@gmail.com>
2021-12-25 19:13:02 +0800

    clear

--- src/views/View.vue
+++ src/views/View.vue
@@ -1,23 +1,11 @@
 <script async setup>
-import { ref, watch, onMounted, reactive } from 'vue'
+import { ref, onMounted, reactive } from 'vue'
 import MigoHandler from '../components/MigoHeader/index.vue'
 import DramaRos from '../components/DramaRos/index.vue'
 import Search from '../components/Search.vue'
 import { switchOffController } from '../controller/switchController.js'
 
-const store = reactive({ searchText: null, series: [] })
-watch(
-  () => store.searchText,
-  (val) => {
-    // console.log('searchText!!', val)
-    // store.series = Lazy(seriesRef.value || [])
-    //   .filter(({ content_type }) => {
-    //     return content_type === 'Series'
-    //   })
-    //   .toArray()
-    store.series = seriesRef.value
-  },
-)
+const store = reactive({ searchText: null })
 
 import axios from 'axios'
 const seriesRef = ref()
@@ -34,7 +22,7 @@ onMounted(async () => {
     <h1>Inventory Manager</h1>
     <Search v-model:inputText="store.searchText" />
     <DramaRos
-      :list="store.series"
+      :list="seriesRef"
       :searchText="store.searchText"
       @switch="switchOffController"
     />
